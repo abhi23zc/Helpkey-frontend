25c0c9ba6ca9037f6a810f4c38cef002d07a28d2
anjali-sharma26 <anjali.sharma.dev26@gmail.com>
2025-06-14 22:51:42 +0530

    feat:ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import Navbar from "./_components/Navbar";
 import Hero from "./_components/Hero";
 import FeaturedDestination from "./_components/FeaturedDestination";
+import ExclusiveOffers from "./_components/ExclusiveOffers";
+import Testimonial from "./_components/Testimonial";
+import NewsLetter from "./_components/NewsLetter";
 
 export default function Home() {
     return (
@@ -10,6 +13,10 @@ export default function Home() {
     <Navbar/>
     <Hero/>
     <FeaturedDestination/>
+    <ExclusiveOffers/>
+    <Testimonial/>
+    <NewsLetter/>
+    
    </>
   );
 }
